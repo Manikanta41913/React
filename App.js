f7d62693b9713424d4fdaef73a60afdac0c6d41c
Manikanta41913 <manikanta41913@gmail.com>
2026-01-06 17:32:16 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,14 @@
+
+const heading = React.createElement("div",
+    { id: "parent" },
+    [React.createElement("div",
+        { id: "child1" },
+        [React.createElement("h1", [], "Hi this tag1 from child1"),
+        React.createElement("h2", [], "Hi this tag2 from child1")]),
+    React.createElement("div", 
+        { id: "child2" },
+        [React.createElement("h1", [], "Hi this tag1 from child2"),
+        React.createElement("h2", [], "Hi this tag2 from child2")])
+    ]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
